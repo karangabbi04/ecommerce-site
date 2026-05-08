5ae26282d693565fe7f7ac5dc8713567349f61e7
karan <karangabbi04@users.noreply.github.com>
2026-05-08 20:53:18 +0530

    add optional auth middelware

--- backend/src/middlewares/verifyJWT.middleware.ts
+++ backend/src/middlewares/verifyJWT.middleware.ts
@@ -1,6 +1,6 @@
 // src/middlewares/auth.middleware.ts
 
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction, response } from "express";
 import jwt from "jsonwebtoken";
 import { prisma } from "../lib/prisma.js";
 import { ApiError } from "../utils/ApiError";
@@ -12,6 +12,7 @@ type JwtPayload = {
 };
 
 export const verifyJWT = asyncHandler(
+  
   async (req: Request, res: Response, next: NextFunction) => {
     const token =
       req.cookies?.accessToken ||
@@ -51,3 +52,47 @@ export const verifyJWT = asyncHandler(
     next();
   }
 );
+
+
+ export const optionalAuth = asyncHandler (async (req: Request, res: Response, next: NextFunction)=>{
+
+  const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+      console.log(req.cookies)
+
+    if (!token) {
+      return next();
+    }
+
+    const decodedToken = jwt.verify(
+      token,
+      process.env.JWT_ACCESS_SECRET 
+    ) as JwtPayload;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: decodedToken.userId,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+        emailVerified: true,
+        createdAt: true,
+      },
+    });
+
+    if (!user) {
+      throw new ApiError(401, "Invalid access token");
+    }
+
+    req.user = user;
+
+    next();
+
+
+
+});
