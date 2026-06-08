61989df2f43c02dcd461d2b70ec3bd499a485873
karan <karangabbi04@users.noreply.github.com>
2026-06-08 18:20:36 +0530

    build address services

---
+++ backend/src/services/address.service.ts
@@ -0,0 +1,105 @@
+import { ApiError } from "@google/genai";
+
+import { ApiResponse } from "../utils/apiResponse";
+import { asyncHandler } from "../utils/asyncHandler";
+import { CreateAddressInput } from "../validations/address.schema";
+import { prisma } from "../lib/prisma";
+
+type CreateAddressParams = {
+  userId?: string;
+  guestId?: string;
+  data: CreateAddressInput;
+};
+
+export const createAddressService = 
+  async ( params: CreateAddressParams) => {
+    
+    const {
+        userId,
+        guestId,
+        data,
+  } = params;
+
+
+        const address = await prisma.address.create({
+      data: {
+
+        userId: userId ?? null,
+
+        guestId: guestId ?? null,
+
+        fullName:
+          data.fullName,
+
+        phone:
+          data.phone,
+
+        addressLine1:
+          data.addressLine1,
+
+        addressLine2:
+          data.addressLine2,
+
+        city:
+          data.city,
+
+        state:
+          data.state,
+        
+        country:
+          "india",
+
+        postalCode:
+          data.pincode,
+
+        latitude:
+          data.latitude,
+
+        longitude:
+          data.longitude,
+      },
+    });
+
+  return address;
+
+  }
+
+export async function getAddresses(
+  userId?: string,
+  guestId?: string
+) {
+
+  return prisma.address.findMany({
+    where: userId
+      ? {
+          userId,
+        }
+      : {
+          guestId,
+        },
+
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+}
+
+export async function attachAddressToCheckout(
+  checkoutId: string,
+  addressId: string
+) {
+
+  const checkout =
+    await prisma.checkoutSession.update({
+      where: {
+        id: checkoutId,
+      },
+
+      data: {
+        addressId,
+      },
+    });
+
+  return checkout;
+}
