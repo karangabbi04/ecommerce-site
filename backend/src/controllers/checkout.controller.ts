6c4e7376d8b685c796c24beeea31829216041ef1
karan <karangabbi04@users.noreply.github.com>
2026-06-30 21:41:27 +0530

    fetch checkout session controller

--- backend/src/controllers/checkout.controller.ts
+++ backend/src/controllers/checkout.controller.ts
@@ -1,7 +1,7 @@
 import { Request,Response } from "express";
 import { ApiResponse } from "../utils/apiResponse";
 import { ApiError } from "../utils/ApiError";
-import { createcheckoutsession } from "../services/checkout.service";
+import { createcheckoutsession, fetchCheckoutSession} from "../services/checkout.service";
 import { asyncHandler } from "../utils/asyncHandler";
 
 export const checkoutController = asyncHandler(async (req: Request, res: Response) => {
@@ -19,3 +19,31 @@ export const checkoutController = asyncHandler(async (req: Request, res: Respons
 
    return  res.status(200).json(new ApiResponse( 200,session,"Checkout session created"));
 });
+
+
+export const fetchCheckoutsession = asyncHandler(async (req: Request, res: Response) => {
+
+     const checkoutId = req.params.checoutId
+
+     console.log(checkoutId)
+
+
+     if (typeof checkoutId !== "string") {
+        throw new ApiError(400,"checkoutID not provided ")
+
+}
+
+
+     if(!checkoutId){
+        throw new ApiError(400,"checkoutID not provided ")
+     }
+
+        const result = await fetchCheckoutSession(checkoutId)
+
+   return res.status(200).json(new ApiResponse( 200,result,"Checkout session created"));
+
+
+})
+
+
+
