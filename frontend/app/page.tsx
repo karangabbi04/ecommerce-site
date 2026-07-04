0da47e0ace1000ac4437320af7b6173f2606eca0
karan <karangabbi04@users.noreply.github.com>
2026-07-04 16:33:43 +0530

    update import link in main page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,14 +1,14 @@
-import HeroSection from "@/components/section/HeroSection";
-import FeaturedProducts from "@/components/section/FeaturedProducts";
-import WhyChooseUs from "@/components/section/WhyChooseUs";
-import Testimonial from "@/components/section/Testimonial";
-import Footer from "@/components/section/Footer";
+import HeroSection from "@/components/Home/heroSection/heroSection";
+import FeaturedProducts from "@/components/Home/feature-product/featured-products";
+import WhyChooseUs from "@/components/Home/why-choose-us/why-choose-us";
+import Testimonial from "@/components/Home/testimonial/index";
+import Footer from "@/components/Home/footer/footer";
 
 export default function Home() {
   return (
     <>
     <main className="relative overflow-hidden bg-gradient-to-b from-[#f5f5f7] via-white to-[#f5f5f7] text-zinc-950">
-   <HeroSection />
+  <HeroSection />
    <FeaturedProducts />
    <WhyChooseUs />
    <Testimonial />
