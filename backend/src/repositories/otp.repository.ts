b8c2e8cef8014f49723fe191a5e0d033af23dc85
karan <karangabbi04@users.noreply.github.com>
2026-07-15 17:45:44 +0530

    create otp repostitory

---
+++ backend/src/repositories/otp.repository.ts
@@ -0,0 +1,97 @@
+// src/modules/otp/otp.repository.ts
+
+import { Prisma, PrismaClient } from "@prisma/client";
+import { OtpPurpose } from "../constants/otp.constants";
+import { prisma } from "../lib/prisma";
+
+
+class OTPRepository {
+  async findLatestOTP( db:PrismaClient | Prisma.TransactionClient,email: string, purpose: OtpPurpose) {
+    return db.emailOTP.findFirst({
+      where: {
+        email,
+        purpose,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  }
+
+  async findRecentOTP(db:PrismaClient | Prisma.TransactionClient,
+    email: string,
+    purpose: OtpPurpose,
+    after: Date
+  ) {
+    return db.emailOTP.findFirst({
+      where: {
+        email,
+        purpose,
+        createdAt: {
+          gt: after,
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  }
+
+  async createOTP(db:PrismaClient | Prisma.TransactionClient,data: {
+    email: string;
+    otpHash: string;
+    purpose: OtpPurpose;
+    expiresAt: Date;
+  }) {
+    return db.emailOTP.create({
+      data,
+    });
+  }
+
+  async deleteOTPById(db:PrismaClient | Prisma.TransactionClient,id: string) {
+    return db.emailOTP.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async deleteOTPByEmail(db:PrismaClient | Prisma.TransactionClient ,
+    email: string,
+    purpose: OtpPurpose
+  ) {
+    return db.emailOTP.deleteMany({
+      where: {
+        email,
+        purpose,
+      },
+    });
+  }
+
+  async incrementAttempts(id: string) {
+    return prisma.emailOTP.update({
+      where: {
+        id,
+      },
+      data: {
+        attempts: {
+          increment: 1,
+        },
+      },
+    });
+  }
+
+  async markVerified(id: string) {
+    return prisma.emailOTP.update({
+      where: {
+        id,
+      },
+      data: {
+        verified: true,
+        varifiedAt: new Date(),
+      },
+    });
+  }
+}
+
+export const otpRepository = new OTPRepository();
