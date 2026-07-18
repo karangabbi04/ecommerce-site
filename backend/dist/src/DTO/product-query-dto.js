fcf4fa4d240fa500998d29979f9fefcd98c2aa40
karan <karangabbi04@users.noreply.github.com>
2026-07-18 18:57:24 +0530

    Remove dist from repository

--- backend/dist/src/DTO/product-query-dto.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.productQuerySchema = void 0;
-var product_validation_1 = require("../validations/product-validation");
-Object.defineProperty(exports, "productQuerySchema", { enumerable: true, get: function () { return product_validation_1.productQuerySchema; } });
--- backend/dist/src/shared/cart/cart-session.constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GUEST_ID_REGEX = exports.GUEST_CART_MAX_AGE = exports.GUEST_CART_COOKIE_NAME = void 0;
-exports.GUEST_CART_COOKIE_NAME = "guest_cart_id";
-exports.GUEST_CART_MAX_AGE = 1000 * 60 * 60 * 24 * 30;
-exports.GUEST_ID_REGEX = /^guest_[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
