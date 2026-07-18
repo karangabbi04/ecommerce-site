10294164f1534bce3a0e28a96c41fad737805794
karan <karangabbi04@users.noreply.github.com>
2026-07-18 19:09:07 +0530

    add doker and downgrade ta prisma version

--- backend/src/scripts/seed.ts
+++ backend/src/scripts/seed.ts
@@ -1,33 +1,33 @@
-import "dotenv/config";
-import { PrismaClient } from "../generated/client.js";
-import { PrismaPg } from "@prisma/adapter-pg";
+// import "dotenv/config";
+// import { PrismaClient } from "../generated/client.js";
+// import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
+// const adapter = new PrismaPg({
+//   connectionString: process.env.DATABASE_URL!,
+// });
 
-const prisma = new PrismaClient({
-  adapter,
-});
+// const prisma = new PrismaClient({
+//   adapter,
+// });
 
-async function main() {
-  const product = await prisma.product.create({
-    data: {
-      name: "Bottle Craft",
-      description: "Handmade decor item",
-      price: 499,
-      category: "Decor",
-      stock: 10,
-      imageUrl: "https://example.com/img.jpg",
-      isFeatured: true,
-    },
-  });
+// async function main() {
+//   const product = await prisma.product.create({
+//     data: {
+//       name: "Bottle Craft",
+//       description: "Handmade decor item",
+//       price: 499,
+//       category: "Decor",
+//       stock: 10,
+//       imageUrl: "https://example.com/img.jpg",
+//       isFeatured: true,
+//     },
+//   });
 
-  console.log("Inserted:", product);
-}
+//   console.log("Inserted:", product);
+// }
 
-main()
-  .catch(console.error)
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+// main()
+//   .catch(console.error)
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
