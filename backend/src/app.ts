7be98da987968ed2368ca31b78e3a1002a7be7c8
karan <karangabbi04@users.noreply.github.com>
2026-07-18 14:08:11 +0530

    add forntend variale using .env

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,7 +18,7 @@ import { errorHandler } from "./middlewares/errorHandler.js";
 const app = express();
 
 app.use(cors(
- { origin: "http://localhost:3000",
+ { origin: process.env.FRONTEND_URL,
   credentials: true,
 }
 ));
