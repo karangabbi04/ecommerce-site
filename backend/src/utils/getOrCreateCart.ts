006fbd2541d1c9bad5b66fdca4b64e00b3875e44
karan <karangabbi04@users.noreply.github.com>
2026-07-19 19:57:39 +0530

    change cookie  option for  crose  site request

--- backend/src/utils/getOrCreateCart.ts
+++ backend/src/utils/getOrCreateCart.ts
@@ -10,7 +10,7 @@ const GUEST_CART_MAX_AGE = 1000 * 60 * 60 * 24 * 30; // 30 days
 
 const guestCartCookieOptions: CookieOptions = {
   httpOnly: true,
-  sameSite: "lax",
+  sameSite: "none",
   secure: process.env.NODE_ENV === "production",
   maxAge: GUEST_CART_MAX_AGE,
 };
