00ace7f703a72361bb4f85d1bc051d4fe5d15ea2
karan <karangabbi04@users.noreply.github.com>
2026-07-25 23:46:40 +0530

    add login otp verify routes

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser,loginUser,getCurrentUser,verifyUserUsingOtp } from "../controllers/user.controller.js";
+import { registerUser,loginUser,getCurrentUser,verifyUserUsingOtp, loginotpverify } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/verifyJWT.middleware.js";
 
 const router = Router();
@@ -8,5 +8,6 @@ router.post("/signup", registerUser);
 router.post("/login", loginUser);
 router.get("/me",verifyJWT,getCurrentUser)
 router.post("/verifyOtp",verifyUserUsingOtp)
+router.post("/loginVerify",loginotpverify)
 
 export default router;
