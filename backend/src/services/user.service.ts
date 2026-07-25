88b6e800e3933c6ff3ac1ad76b8f51599441036a
karan <karangabbi04@users.noreply.github.com>
2026-07-25 23:46:22 +0530

    add login service  and otp verify  during user login

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -10,7 +10,7 @@ import {
 import { userRepository } from "../repositories/user.repository.js";
 
 import { redisService } from "../services/redis.service.js";
-
+import { TempAuthUser } from "../types/user.types.js";
 import {
     RegisterUserDto,
     LoginUserDto,
@@ -19,6 +19,7 @@ import {
 import { otpService } from "./otp.service.js";
 import { OtpPurpose } from "@prisma/client";
 import { otpRepository } from "../repositories/otp.repository.js";
+import { number } from "zod";
 
 
 const SIGNUP_CACHE_PREFIX = "signup";
@@ -129,4 +130,85 @@ const signupData=await redisService.get<SignupCache>(key);
                     };
 
 } 
+
+export const  loginService = async (email:string,password:string) => {
+
+    const user = await userRepository.findByEmail(email) 
+
+   if (!user || !user.password) {
+    throw new ApiError(400, "Invalid email or password");
+  }
+
+  const isPasswordValid = await comparePassword(password, user.password);
+
+    if (!isPasswordValid) {
+    throw new ApiError(400, "Invalid   password");
+  }
+
+  await redisService.set(
+    `auth:user:${user.email}`,
+    {
+             id: user.id,
+            name: user.name,
+            email: user.email,
+            phone:user.phone,
+            emailVerified: user.emailVerified,
+            createdAt: user.createdAt,
+    },
+    300
+);
+
+  await otpService.sendOTP({email, purpose:OtpPurpose.LOGIN});
+
+            return 
+
+
+}
+
+export const  verifyLogin = async (email:string,otp:string) => {
+
+    await otpService.verifyOTP({email,otp,purpose:OtpPurpose.LOGIN});
+
+
+        const user =
+        await redisService.get<TempAuthUser>(
+            `auth:user:${email}`
+        );
+
+    if(!user){
+    throw new ApiError(400, "login session expird or cached ");
+
+    }   
+
+           const userData = {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            phone:user.phone,
+            emailVerified: user.emailVerified,
+            createdAt: user.createdAt,
+            };
+
+    await otpRepository.deleteOTPByEmail(prisma,email,OtpPurpose.LOGIN);
+
+    
+        const accessToken = generateAccessToken({
+                            userId: user.id,
+                            email: user.email,
+                        });
+
+                        const refreshToken = generateRefreshToken({
+                            userId: user.id,
+                            email: user.email,
+                        });
+
+                    return {userData,
+                        accessToken,refreshToken
+
+
+
+
+}
+}
+
 
