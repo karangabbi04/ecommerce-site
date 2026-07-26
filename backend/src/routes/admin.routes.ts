cc4a81589dce08c1db0ac8e0f1de5fe53586dca5
karan <karangabbi04@users.noreply.github.com>
2026-07-26 23:15:20 +0530

    add login and verify login routes for admin

--- backend/src/routes/admin.routes.ts
+++ backend/src/routes/admin.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-
+import { adminController } from "../controllers/admin.controller";
+import { verifyJWT } from "../middlewares/verifyJWT.middleware";
 const router =Router()
 
-// router.get("/orders",)
+router.post("/login",adminController.adminLogin)
+
+router.post("/verify",adminController.adminVerify)
+router.get("/orders",verifyJWT,adminController.orderInfo)
 
 
 export default router
