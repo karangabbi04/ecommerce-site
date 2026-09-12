4df911d0ac67c03a9db6915bc4d22daee8ffe1b7
karan <karangabbi04@users.noreply.github.com>
2026-09-12 17:30:14 +0530

    add some admin route

--- backend/src/routes/admin.routes.ts
+++ backend/src/routes/admin.routes.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
-import { adminController } from "../controllers/admin.controller";
+import { adminController } from "../controllers/Analytics.controller";
 import { verifyJWT } from "../middlewares/verifyJWT.middleware";
+import { createCategory,deleteCategory } from "../controllers/category.controller";
 const router =Router()
 
 router.post("/login",adminController.adminLogin)
 
 router.post("/verify",adminController.adminVerify)
-router.get("/orders",verifyJWT,adminController.orderInfo)
+router.get("/orders",adminController.orderInfo)
+router.get("/revenue",adminController.revenueDetail)
+
+router.post("/add-category",createCategory)
+router.delete("/categories/:id",deleteCategory);
+
 
 
 export default router
